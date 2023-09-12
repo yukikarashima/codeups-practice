aace375a8eeecda708c2e4b082341c7dbc32dfd1
yuki <ckaracw@gmail.com>
2023-09-12 15:17:07 +0900

    画像のアニメーション追加

--- src/js/script.js
+++ src/js/script.js
@@ -42,5 +42,31 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     }
 });
 
+//要素の取得とスピードの設定
+var box = $('.colorbox'),
+    speed = 700;  
+
+//.colorboxの付いた全ての要素に対して下記の処理を行う
+box.each(function(){
+    $(this).append('<div class="color"></div>')
+    var color = $(this).find($('.color')),
+    image = $(this).find('img');
+    var counter = 0;
+
+    image.css('opacity','0');
+    color.css('width','0%');
+    //inviewを使って背景色が画面に現れたら処理をする
+    color.on('inview', function(){
+        if(counter == 0){
+        $(this).delay(200).animate({'width':'100%'},speed,function(){
+                image.css('opacity','1');
+                $(this).css({'left':'0' , 'right':'auto'});
+                $(this).animate({'width':'0%'},speed);
+                })
+            counter = 1;
+        }
+    });
+});
+
 });
 
